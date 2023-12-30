cc6ced1e17cb2fbe424d7e6edd5a276986f6cef3
Yong Lim <yong.lim@gmail.com>
2023-12-30 10:47:04 +0700

    Make contact form works with Google Form

---
+++ assets/js/postAOCAM.js
@@ -0,0 +1,57 @@
+$(function() {
+   $('.form-control').focus(formFocus);
+ });
+ 
+function formFocus() {
+  $('#alert-field')
+    .removeClass()
+    .addClass('is-hidden');
+ }
+
+function formReset() {
+  $('#alert-field')
+    .removeClass()
+    .html("<span><p>Thank you for dropping us a line . . .</p></span>")
+    .removeClass('is-hidden');
+  document.getElementById("my-form").reset();
+  setTimeout(() => {
+    $('#alert-field').removeClass().addClass('is-hidden');
+    // console.log('This alert appeared after 3 second!'); 
+  }, 3000);
+}
+
+//selector from your HTML form
+function postAOCAM(e) {
+  //prevent the form from submiting so we can post to the google form
+  e.preventDefault();
+  console.log("in postAOCAM");
+  $('#alert-field')
+    .removeClass()
+    .html("<span><p>Please wait while we're registering you . . .</p>  <progress></progress></span>")
+    .removeClass('is-hidden');
+  // console.log('start timer');
+  // setTimeout(() => {
+  //   console.log('This alert appeared after 3 second!'); }, 3000);
+  //AJAX request
+  $.ajax({
+   url: 'https://docs.google.com/forms/d/e/1FAIpQLSfPY4dm1DZQe4C-9o_L7-uhCb5Ci0ISXngc27UHJlUpdtY5xg/formResponse',     //The public Google Form url, but replace /view with /formResponse
+   data: $('#my-form').serialize(), //Nifty jquery function that gets all the input data 
+   type: 'POST', //tells ajax to post the data to the url
+   dataType: "json", //the standard data type for most ajax requests
+   statusCode: { //the status code from the POST request
+     0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+      //success
+      formReset();
+     }, 
+     200: function(data) {//200 is a success code. it went through!
+       //success
+       // $('#form-success').text('hooray! 200');
+      formReset();
+     },
+     403: function(data) {//403 is when something went wrong and the submission didn't go through
+       //error
+       alert('Oh no! something went wrong. Please let us know of your problem.');
+     }
+   }  
+  });
+};
