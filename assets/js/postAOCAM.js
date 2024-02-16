990ba5d968668e65b0c86c1beb8edd673a6791f5
Yong Lim <yong.lim@gmail.com>
2024-02-16 11:57:00 +0700

    Change the Google Form file to aocam's owned

--- assets/js/postAOCAM.js
+++ assets/js/postAOCAM.js
@@ -27,31 +27,32 @@ function postAOCAM(e) {
   console.log("in postAOCAM");
   $('#alert-field')
     .removeClass()
-    .html("<span><p>Please wait while we're registering you . . .</p>  <progress></progress></span>")
+    .html("<span><p>Please wait while we're sending your messgae . . .</p>  <progress></progress></span>")
     .removeClass('is-hidden');
   // console.log('start timer');
   // setTimeout(() => {
   //   console.log('This alert appeared after 3 second!'); }, 3000);
   //AJAX request
   $.ajax({
-   url: 'https://docs.google.com/forms/d/e/1FAIpQLSfPY4dm1DZQe4C-9o_L7-uhCb5Ci0ISXngc27UHJlUpdtY5xg/formResponse',     //The public Google Form url, but replace /view with /formResponse
-   data: $('#my-form').serialize(), //Nifty jquery function that gets all the input data 
-   type: 'POST', //tells ajax to post the data to the url
-   dataType: "json", //the standard data type for most ajax requests
-   statusCode: { //the status code from the POST request
-     0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
-      //success
-      formReset();
-     }, 
-     200: function(data) {//200 is a success code. it went through!
+    url: 'https://docs.google.com/forms/d/e/1FAIpQLSfSSrMZh4gd_b-u08p0ZmWSzaLRlJfVobR8dRhHe4PlIAaEsw/formResponse',     
+    //The public Google Form url, but replace /view with /formResponse
+    data: $('#my-form').serialize(), //Nifty jquery function that gets all the input data 
+    type: 'POST', //tells ajax to post the data to the url
+    dataType: "json", //the standard data type for most ajax requests
+    statusCode: { //the status code from the POST request
+      0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
        //success
-       // $('#form-success').text('hooray! 200');
-      formReset();
-     },
-     403: function(data) {//403 is when something went wrong and the submission didn't go through
-       //error
-       alert('Oh no! something went wrong. Please let us know of your problem.');
-     }
-   }  
+        formReset();
+       }, 
+       200: function(data) {//200 is a success code. it went through!
+         //success
+         // $('#form-success').text('hooray! 200');
+        formReset();
+       },
+       403: function(data) {//403 is when something went wrong and the submission didn't go through
+         //error
+         alert('Oh no! something went wrong. Please let us know of your problem.');
+       }
+    }  
   });
 };
